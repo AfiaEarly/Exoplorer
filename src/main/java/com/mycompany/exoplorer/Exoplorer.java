365bdfeeb4e2b281bcf2d11ddb5b0d591f4526ae
AfiaEarly <afiazearly@gmail.com>
2026-02-18 13:20:29 +0600

    Final commit

---
+++ src/main/java/com/mycompany/exoplorer/Exoplorer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exoplorer;
+
+/**
+ *
+ * @author USER
+ */
+public class Exoplorer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
